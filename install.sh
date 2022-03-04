f71b683ccfc505cc71761152ffad436509a37352
S59MZ <matjaz.zibert1@gmail.com>
2022-03-04 16:17:11 +0100

    first commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+# Install the project...
+
+mkdir /usr/lib/cmake/fpga_ai/
+cp ../gr-fpga_ai/cmake/Modules/fpga_aiConfig.cmake /usr/lib/cmake/fpga_ai/
+
+mkdir /usr/include/fpga_ai/
+cp ../gr-fpga_ai/include/fpga_ai/api.h /usr/include/fpga_ai/
+
+mkdir /usr/lib/python3/dist-packages/fpga_ai/
+cp ../gr-fpga_ai/python/*.py /usr/lib/python3/dist-packages/fpga_ai/
+
+cp ../gr-fpga_ai/grc/*.yml /usr/share/gnuradio/grc/blocks/
+
---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+#Scanning dependencies of target uninstall
+
+rm -rf /usr/lib/cmake/fpga_ai
+rm -rf /usr/include/fpga_ai
+rm -rf /usr/lib/python3/dist-packages/fpga_ai
+rm /usr/share/gnuradio/grc/blocks/fpga_ai_rf_classf.block.yml
+rm /usr/share/gnuradio/grc/blocks/fpga_ai_sample_mod_src.block.yml
+
